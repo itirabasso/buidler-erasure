8d0e2aefb69c1bf7b887ca5ec205e7a4df8753c8
Ignacio M. Tirabasso <ignacio.tirabasso@gmail.com>
2019-11-10 14:05:02 -0300

    remove unused boilerplate stuff

--- src/type-extensions.d.ts
+++ src/type-extensions.d.ts
@@ -2,12 +2,8 @@ import "@nomiclabs/buidler/types";
 import { ErasureDeploySetup } from "./erasureSetup";
 import { Contract } from "ethers";
 
-// This file is used to extend Buidler's types. Most plugins contain a
-// src/type-extensions.d.ts, so we recommend to keep this name.
-
 declare module "@nomiclabs/buidler/types" {
-  // This is an example of an extension to the Buidler Runtime Environment.
-  // This new field will be available in tasks' actions, scripts, and tests.
+
   export interface BuidlerRuntimeEnvironment {
     deployments: {
       getDeployedAddresses(name: string): Promise<string[]>;
@@ -15,10 +11,5 @@ declare module "@nomiclabs/buidler/types" {
       saveDeployedContract(name: string, instance: any): void;
       deploySetup: ErasureDeploySetup;
     };
-  }
-
-  // This is an example of an extension to one of the Buidler config values.
-  export interface ProjectPaths {
-    newPath?: string;
   }
 }
